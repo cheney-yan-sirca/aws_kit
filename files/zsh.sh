d605cf41a495f403fcd172c8531a6f3d2090942b
cheney.yan <cheney.yan@sirca.org.au>
2015-04-23 17:07:19 +1000

    Minor fixes

--- files/zsh.sh
+++ files/zsh.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env zsh
 
-rm -rf ~/.boto;
-mkdir ~/bin;
-mkdir ~/data;
 mkdir ~/work;
 
 cd ~
 
-mkdir -p ~/.config
-cd ~
 mv _tmux.conf .tmux.conf
 mkdir .tmuxinator
 mv prepare-env.py powerline.sh env.json .tmuxinator
 
-mv *pem ~/.ssh
-chmod 600 ~/.ssh/*
 cat >> ~/.ssh/config << EOL
 UserKnownHostsFile /dev/null
 LogLevel QUIET              
@@ -26,10 +19,8 @@ mv _rpmmacros .rpmmacros
 mv ~/rsync_remote.sh ~/bin
 ~/.tmuxinator/prepare-env.py init-layout
 
-
 #=============== using zsh============================================
 sed -i 's/bashrc/zshrc/g' ~/.zshrc
-sed -i 's/ZSH_THEME.*/ZSH_THEME="agnoster"/g' ~/.zshrc
 wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
 mkdir ~/.oh-my-zsh/completions
 mv ~/_mux ~/.oh-my-zsh/completions
@@ -40,3 +31,5 @@ echo >> ~/.zshrc << EOF
 export PATH="$PATH:$HOME/bin"
 #unsetopt share_history
 EOF
+
+sed -i 's/ZSH_THEME.*/ZSH_THEME="agnoster"/g' ~/.zshrc
