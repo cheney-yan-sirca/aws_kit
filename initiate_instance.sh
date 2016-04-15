2a52b2fe4f8f730b4ce81d7f8db5aba48ff121d9
cheney.yan <cheney.yan@sirca.org.au>
2016-04-15 12:43:40 +1000

    correct block device mapping

--- initiate_instance.sh
+++ initiate_instance.sh
@@ -34,11 +34,15 @@ export name=$(aws ec2 describe-images --filters Name=name,Values="${IMG}" | grep
 echo "${name} is the latest $IMG iamge."
 export image_id=$(aws ec2 describe-images --filters Name=name,Values=${name} | grep '"ImageId"' | awk -F'"' '{print $4}' | sort | tail -1 | xargs )
 echo "${image_id} is the iamge id for ${name}."
-if [ ${image_id} == "" ]; then
+if [ "${image_id}" == "" ]; then
     echo "Something is wrong! Could not figure out image_id"
     exit 1
 fi
 instance_id=$(aws ec2 run-instances --image-id ${image_id} $EBS_OPTIONS --key-name $SSH_KEY_NAME --security-groups  remote-working-desktop  --iam-instance-profile Name=$INSTANCE_PROFILE --instance-initiated-shutdown-behavior stop  --instance-type $instance_type | grep '"InstanceId"' | awk -F'"' '{print $4}' |xargs)
+if [ "${instance_id}" == "" ]; then
+        echo "Something is wrong! Could not create instance"
+        exit 1
+fi
 echo "${instance_id} is the newly generated instance id."
 aws ec2 create-tags --resource ${instance_id} --tags Key=Name,Value=${user_name}-desktop
 aws ec2 create-tags --resource ${instance_id} --tags Key=Billing,Value=${use}
