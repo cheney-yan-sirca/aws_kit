e67dfc0c5e6e34f43bbeec3992496d6ea8bf9087
Cheney Yan <cheney.yan@sirca.org.au>
2018-02-09 22:36:02 +1100

    Regular check ins.

--- files/setup_user.sh
+++ files/setup_user.sh
@@ -9,6 +9,14 @@ LogLevel QUIET
 StrictHostKeyChecking no
 EOL
 chmod 600  ~/.ssh/*
-virtualenv -p /usr/bin/python27 ~/py27
+
+
+
+virtualenv -p /usr/bin/python ~/py27
 source ~/py27/bin/activate
-pip install --upgrade pycli simplejson pyaml godaddypy aws-ec2-assign-elastic-ip requests[security] boto3 thefuck pyCLI
+pip install --upgrade pycli simplejson pyaml godaddypy aws-ec2-assign-elastic-ip requests[security] boto3 pyCLI ptpython
+
+deactivate
+
+virtualenv -p /usr/bin/python3 ~/py3
+
--- files/softwares.sh
+++ files/softwares.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
-#yum groupinstall -y 'Development Tools' # not happy with py27?
-yum install -y git ruby ruby-devel rubygems gcc mysql-devel -y bash-completion zsh tmux mosh rlwrap jq --enablerepo epel
+yum groupinstall -y 'Development Tools' # not happy with py27?
+yum install -y git ruby ruby-devel rubygems gcc mysql-devel python-devel bash-completion zsh tmux mosh rlwrap jq # --enablerepo epel
 gem install io-console
 pip install --upgrade awscli pyCLI boto3 requests[security] pylint tox twine thefuck godaddypy
 # working environment tools
 gem install tmuxinator -v 0.6.8
 gem install papertrail
+mazon-linux-extras install python3
 
 echo "ServerAliveInterval 10" | tee -a /etc/ssh/ssh_config
 
@@ -19,4 +20,21 @@ service docker start
 sudo usermod -a -G docker ec2-user
 
 # a patch for the compinit error
-chmod a-r /etc/profile.d/aws-cli.sh
+#chmod a-r /etc/profile.d/aws-cli.sh
+
+easy_install pip
+pip install virtualenv
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+
+
+#--------mosh
+sudo yum -y install autoconf automake gcc gcc-c++ make boost-devel zlib-devel ncurses-devel protobuf-devel openssl-devel
+cd /usr/local/src
+sudo wget http://mosh.mit.edu/mosh-1.2.4.tar.gz
+sudo tar xvf mosh-1.2.4.tar.gz
+cd mosh-1.2.4
+sudo ./autogen.sh
+sudo ./configure
+sudo make
+sudo make install
+#----------------
